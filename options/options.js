a58f236be2562439d8f0894d2b71e48ead586a29
Crowdbotics <team@crowdbotics.com>
2023-11-16 19:15:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "logista-test-2-44598",
+  url: "https://logista-test-2-44598.botics.co",
+  api: "https://logista-test-2-44598.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
